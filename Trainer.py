0d39ca279afd84d35d0be4a14af1ee7bbbe59262
Atubey7 <ayaanbeig2009@gmail.com>
2026-08-28 20:02:43 +0400

    Fixed some syntax and changed it from activate_gimmick to activate_form

--- Trainer.py
+++ Trainer.py
@@ -4,7 +4,7 @@ class Trainer:
         self.pokemon_team = pokemon_team
         self.active_pokemon = 0
         self.gimmick_used = False
-    def getactive(self):
+    def get_active(self):
         return self.pokemon_team[self.active_pokemon]
     def next_healthy(self):
         for i, pokemon in enumerate(self.pokemon_team):
@@ -13,13 +13,13 @@ class Trainer:
                 return True
         return False
     def switch_pokemon(self, index):
-        if self.pokemon_team[index].health != 0:
+        if self.pokemon_team[index].Health != 0:
             self.active_pokemon = index
         else:
             self.next_healthy()
     def get_gimmick(self):
         return self.pokemon_team[self.active_pokemon].current_form
-    def set_gimmick(self, gimmick):
+    def set_gimmick(self, form):
         self.gimmick_used = True
-        self.pokemon_team[self.active_pokemon].current_form = gimmick
-        self.pokemon_team[self.active_pokemon].activate_gimmick(gimmick)
+        self.pokemon_team[self.active_pokemon].current_form = form
+        self.pokemon_team[self.active_pokemon].activate_form(form)
