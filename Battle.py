1187f6ab24018a8c112a3df3d1509baa2eb434c4
Atubey7 <ayaanbeig2009@gmail.com>
2026-08-29 13:56:55 +0400

    Added Crit Chance Calculations to damage calculator. Added a basic AI to choose the move with the largest damage potential. Fixed logic for player faint

--- Battle.py
+++ Battle.py
@@ -12,8 +12,6 @@ def check_faint(trainer):
 
 def gimmick_choice(trainer):
     pass
-def move_choice(trainer):
-    return trainer.get_active().moves[0]
 def battle_text(attacker, attacker_move, defender):
     effectiveness = pokemon_type_effectiveness(attacker_move, defender.get_active())
     text = ""
@@ -52,25 +50,53 @@ def calculate_damage(attacker, defender, move, attacker_gimmick):
 
     variance = random.uniform(0.85, 1.0)
 
-    damage = round(base * stab * variance * type)
+    #Crit Chance
+    modifiers = 0
+    crit_stage = move.crit + modifiers
+    if crit_stage == 0.0:
+        chance = 24
+    elif crit_stage == 1.0:
+        chance = 8
+    elif crit_stage == 2.0:
+        chance = 2
+    elif crit_stage >= 3.0:
+        chance = 1
+    num = random.randint(1, chance)
+    crit = 2 if num == 1 else 1
+
+    damage = round(base * stab * variance * type * crit)
     return damage
+
+def move_choice(trainer, opponent, attacker_gimmick):
+    if trainer.name == "AI":
+        list = []
+        for move in trainer.get_active().moves:
+            dmg = calculate_damage(trainer.get_active(), opponent.get_active(), move, attacker_gimmick)
+            list.append((dmg, move))
+        best_move = max(list, key=lambda x: x[0])
+        return best_move[1]
+    else:
+        return trainer.get_active().moves[0]
+
+
 def run_battle(player, opponent):
     while True:
         player_gimmick = gimmick_choice(player)
-        player_move = move_choice(player)
+        player_move = move_choice(player, opponent, player_gimmick)
         opponent_gimmick = gimmick_choice(opponent)
-        opponent_move = move_choice(opponent)
+        opponent_move = move_choice(opponent, player, opponent_gimmick)
         equal = False
         if player.get_active().SPEED == opponent.get_active().SPEED and player_move.priority == opponent_move.priority:
             equal = True
             num = random.randint(1,2)
-        if (player.get_active().SPEED > opponent.get_active().SPEED and player_move.priority >= opponent_move.priority) or (equal and num == 1) or player.getactive().priority > opponent.get_active().priority:
+        if (player.get_active().SPEED > opponent.get_active().SPEED and player_move.priority >= opponent_move.priority) or (equal and num == 1) or player.get_active().priority > opponent.get_active().priority:
             player_damage = calculate_damage(player.get_active(), opponent.get_active(), player_move, player_gimmick)
             opponent.get_active().Health -= player_damage
             battle_text(player, player_move, opponent)
             print(f"It dealt: {player_damage}")
             if check_faint(opponent) != "battle_over":
                 opponent_damage = calculate_damage(opponent.get_active(), player.get_active(), opponent_move, opponent_gimmick)
+                player.get_active().Health -= opponent_damage
                 battle_text(opponent, opponent_move, player)
                 print(f"It dealt: {opponent_damage}")
         elif (player.get_active().SPEED < opponent.get_active().SPEED and player_move.priority >= opponent_move.priority) or (equal and num == 2):
@@ -85,5 +111,8 @@ def run_battle(player, opponent):
                 print(f"It dealt: {player_damage}")
         if check_faint(opponent) == "battle_over":
             print(f"{opponent.get_active().name} fainted\n{player.name} wins!")
+            break
+        if check_faint(player) == "battle_over":
+            print(f"{player.get_active().name} fainted\n{opponent.name} wins!")
             break
 
